ab1a7b87ce0f106f9ee871a28f739245158f7d45
aousabdo <aousabdo@gmail.com>
2015-02-05 13:59:51 -0500

    Added controls for the exposure levels

--- ui.R
+++ ui.R
@@ -9,16 +9,27 @@ shinyUI(pageWithSidebar(
   sidebarPanel(
     br(),
     wellPanel(
-      sliderInput("N","Number of Simulated Personas:", min = 100, max = 10000, value = 1000,step = 100 ),
+      sliderInput("N","Number of Simulated Personas:", min = 100, max = 10000, value = 1000,step = 100 )
       #     sliderInput("encounter","Number of Simulated Encounters:", min = 1, max = 10, value = 2,step = 1),
-      br(),
-      sliderInput("ELminor", "% of Minor exposure level", min = 0, max = 100, value = 33, step = 1),
-      sliderInput("ELmoderate", "% of Moderate exposure level", min = 0, max = 100, value = 33, step = 1),
-      sliderInput("ELhigh", "% of High exposure level", min = 0, max = 100, value = 33, step = 1)
-    ), 
-    br(),
-    downloadButton('downloadData', 'Download Simulated Data'),
-    helpText("Download button is not yet working.")
+    ),
+    wellPanel(
+      helpText("Exposure Level: The three sliders provided below allow the user to adjust the percentages of the three
+               different levels of exposure."),
+      helpText(HTML("<p><font color=\"red\"> <b>Please note that percentages selected have to add up to a 100.</b></font></p>")),
+      sliderInput("exp.level.minor", "% of Minor exposure level", min = 1, max = 98, value = 30, step = 1),
+      sliderInput("exp.level.moderate", "% of Moderate exposure level", min = 1, max = 98, value = 40, step = 1),
+      sliderInput("exp.level.high", "% of High exposure level", min = 1, max = 98, value = 30, step = 1)
+    ),
+    
+    wellPanel(
+      p("Click the button to generate the simulations."),
+      actionButton("goButton", "Run!")
+    ),
+    
+    wellPanel(
+      downloadButton('downloadData', 'Download Simulated Data'),
+      helpText("Download button is not yet working.")
+    )
   ),
   
   mainPanel(
