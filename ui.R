9d61e204e655299d6c8937ce27378a8f801afe64
aousabdo <aousabdo@gmail.com>
2015-02-06 16:52:27 -0500

    Added functionality to adjust numbers in transmission prob matrix

--- ui.R
+++ ui.R
@@ -9,22 +9,23 @@ shinyUI(pageWithSidebar(
   sidebarPanel(
     br(),
     wellPanel(
-      sliderInput("N","Number of Simulated Personas:", min = 100, max = 10000, value = 1000,step = 100 ),
+      sliderInput("N","Number of Simulated Personas:", min = 100, max = 1000, value = 150, step = 10),
       br(),
-      sliderInput("encounter","Number of Simulated Encounters:", min = 1, max = 10, value = 3,step = 1),
+      sliderInput("encounter","Number of Simulated Encounters:", min = 1, max = 20, value = 3, step = 1),
       br(),
       checkboxInput("adjustMatrix", label = "Adjust Percentages in Matrix", value = FALSE),
-      conditionalPanel("adjustMatrix == 'TRUE'",
-                       sliderInput("adjustMatrixValue", "Subtract the following value from the Probabilty Matrix:", 
+      conditionalPanel("input.adjustMatrix == true",
+                       sliderInput("adjustMatrixValue", "Subtract the following value from non-zero 
+                                   values in the Transmission Probabilty Matrix:", 
                                    min = 5, max = 50, value = 20, step = 5))
     ),
     wellPanel(
       helpText("Exposure Level: The three sliders provided below allow the user to adjust the percentages of the three
                different levels of exposure."),
       helpText(HTML("<p><font color=\"red\"> <b>Please note that percentages selected have to add up to a 100.</b></font></p>")),
-      sliderInput("exp.level.minor", "% of Minor exposure level", min = 1, max = 98, value = 30, step = 1),
-      sliderInput("exp.level.moderate", "% of Moderate exposure level", min = 1, max = 98, value = 40, step = 1),
-      sliderInput("exp.level.high", "% of High exposure level", min = 1, max = 98, value = 30, step = 1)
+      sliderInput("exp.level.minor", "% of Minor exposure level"      , min = 10, max = 80, value = 30, step = 1),
+      sliderInput("exp.level.moderate", "% of Moderate exposure level", min = 10, max = 80, value = 40, step = 1),
+      sliderInput("exp.level.high", "% of High exposure level"        , min = 10, max = 80, value = 30, step = 1)
     ),
     
     wellPanel(
