d0383f83f55a05890c75a99bb434975806978dd6
aousabdo <aousabdo@gmail.com>
2015-02-09 21:35:33 -0500

    Modified limits

--- ui.R
+++ ui.R
@@ -23,7 +23,7 @@ shinyUI(pageWithSidebar(
       helpText("Exposure Level: The three sliders provided below allow the user to adjust the percentages of the three
                different levels of exposure."),
       helpText(HTML("<p><font color=\"red\"> <b>Please note that percentages selected have to add up to a 100.</b></font></p>")),
-      sliderInput("exp.level.minor", "% of Minor exposure level"      , min = 10, max = 80, value = 30, step = 1),
+      sliderInput("exp.level.minor", "% of Minor exposure level"      , min = 10, max = 80, value = 31, step = 1),
       sliderInput("exp.level.moderate", "% of Moderate exposure level", min = 10, max = 80, value = 40, step = 1),
       sliderInput("exp.level.high", "% of High exposure level"        , min = 10, max = 80, value = 30, step = 1)
     ),
