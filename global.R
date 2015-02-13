cb4a8f56bf7e11255d174f5ae5f01d11422034ab
aousabdo <aousabdo@gmail.com>
2015-02-13 17:04:27 -0500

    Added function to simulate the population with modifiers

--- global.R
+++ global.R
@@ -19,7 +19,7 @@ rownames(transProbMatrix) <- paste("Recipient HS ", rep(1:6), sep = "")
 
 
 # function to simulate the spread of the disease
-simDiseaseTrans <- function(recipient = 3, exposer = 5, exposure = 2, probMatrix){
+simDiseaseTrans <- function(recipient = 3, exposer = 5, exposure = 2, homeState = 2, probMatrix){
   # recipient and exposer take values between 1 and 6
   if(recipient < 1 | recipient > 6){stop("Recipient Health Status has to be in the range 1-6")}
   if(exposer  < 1 | exposer > 6){stop("Exposer Health Status has to be in the range 1-6")}
@@ -29,6 +29,7 @@ simDiseaseTrans <- function(recipient = 3, exposer = 5, exposure = 2, probMatrix
   
   # read omega, likelihood of advancing to next level of health status from dataframe
   omega <- probMatrix[recipient, ((exposer-1)*3 + exposure)]
+  omega <- omega + homeStateWeight[[homeState, 2]]
   
   # if omega is larger than a randomly selected number between 1 and a 100 then
   # the recipient will advance to the next level of health status (become sicker)
@@ -38,7 +39,6 @@ simDiseaseTrans <- function(recipient = 3, exposer = 5, exposure = 2, probMatrix
   
   return(recipient.new)
 }
-
 
 # # function to simulate the spread of the disease bidirectional. NOT USED YET
 # simDiseaseTransBi <- function(recipient = 3, exposer = 5, exposure = 2, probMatrix){
@@ -78,7 +78,45 @@ interchange2V <- function(v1, v2){
   as.vector(rbind(v1, v2))
 }
 
-Age <- c(sample(10:20, 0.25*N), sample(21:60, 0.55*N), sample(61:81, 0.2*N))
+# function to simulate the population with modifiers
+simPop <- function(N = 100, homeState, Age){
+  # simulate the population
+  healthStatus <- c(rep(1:4, N*0.2) ,rep(5:6, N*0.1))
+  # randomize the population  
+  healthStatus <- sample(healthStatus)
+  popDT <- data.table("id" = sample(1e4:9e4, N), "age" = sample(Age(N)), "homestate" = homeState(N), "healthstatus" = healthStatus)
+  popDT[, "hascomorbidity" := sapply(healthstatus, hasComorbidity)]
+  popDT[, "comorbidity" := sapply(hascomorbidity, comorbidity)]
+  return(popDT)
+}
+
+# age distribution
+Age <- function(N){
+  c(sample(10:20, 0.25*N, replace = TRUE), sample(21:60, 0.55*N, replace = TRUE), sample(61:81, 0.2*N, replace = TRUE))
+}
+
+# function to simulate home state google flue trend status
+homeState <- function(N) sample(1:3, N, replace = TRUE)
+
+# dataframe containing home state google flue trend statusus and their weights
+homeStateWeight <- data.frame("homeState" = 1:3, "homeStateWeight" = c(0, 2, 4))
+
+# function to simulate the existance of comorbidity according to health status
+hasComorbidity <- function(healthStatus){
+  if(healthStatus == 1 | healthStatus == 2) hasComorbidity <- rbinom(n = 1, size = 1, prob = 0.18)
+  else hasComorbidity <- rbinom(n = 1, size = 1, prob = 0.5)
+  return(hasComorbidity)
+}
+
+# function to assign comorbidities 
+comorbidity <- function(hasComorbidity){
+  if(!hasComorbidity) comorbidity <- NA
+  else comorbidity <- {
+    sample(c("Diabetes", "CHF", "COPD", "Immunosuppressed", "Cancer",	"Renal", "Transplant", "Cystic Fibrosis"), 1)
+  }
+  return(comorbidity)
+}
+
 
 # common theme for the ggplots
 commonTheme <- theme(axis.text.x = element_text(angle=0, hjust=1, size = 14),
